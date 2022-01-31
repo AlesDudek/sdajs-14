c93c91c8c50d4df36ce0a4a2592eb3f74be5f1b4
Aleš <mit.mit@seznam.cz>
2022-01-31 17:12:18 +0100

    Prerovnani

---
+++ Task_01/script.js
@@ -0,0 +1,152 @@
+const version1 = [
+"On the first day of Christmas, my true love sent to me", 
+"A partridge in a pear tree"
+]
+
+
+const version2 = [ 
+    "On the second day of Christmas, my true love sent to me", 
+    "Two turtle doves, and   A partridge in a pear tree",
+    
+]
+
+const version3 = [
+"On the third day of Christmas, my true love sent to me",   
+"Three french hens",
+"Two turtle doves, and",
+"A partridge in a pear tree",
+]
+
+const version4 = [
+    "On the fourth day of Christmas, my true love sent to me", 
+    "Four calling birds", 
+    "Three french hens",  
+    "Two turtle doves, and", 
+    "A partridge in a pear tree",
+    
+]
+
+const version5 = [
+    
+        "On the fifth day of Christmas, my true love sent to me",
+        "Five golden rings",
+        "Four calling birds",
+        "Three french hens",
+        "Two turtle doves, and",
+        "A partridge in a pear tree",
+    
+]
+
+const version6 = [
+    "On the sixth day of Christmas, my true love sent to me", 
+    "Six geese a-laying",  
+    "Five golden rings",   
+    "Four calling birds",   
+    "Three french hens",
+    "Two turtle doves, and",  
+    "A partridge in a pear tree",
+    
+]
+
+const version7 = [
+    "On the seventh day of Christmas, my true love sent to me",
+    "Seven swans a-swimming", 
+    "Six geese a-laying", 
+    "Five golden rings", 
+    "Four calling birds", 
+    "Three french hens",   
+    "Two turtle doves, and",   
+    "A partridge in a pear tree",
+    
+]
+
+const version8 = [
+        "On the eighth day of Christmas, my true love sent to me",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three french hens",
+        "Two turtle doves, and",
+        "A partridge in a pear tree",
+]
+
+const version9 = [
+    "On the ninth day of Christmas, my true love sent to me", 
+    "Nine ladies dancing",    
+    "Eight maids a-milking", 
+    "Seven swans a-swimming",   
+    "Six geese a-laying", 
+    "Five golden rings",    
+    "Four calling birds",  
+    "Three french hens", "Two turtle doves, and",    
+    "A partridge in a pear tree",
+    
+]
+
+const version10 = [
+"On the tenth day of Christmas, my true love sent to me",
+"Ten lords a-leaping",
+"Nine ladies dancing",
+"Eight maids a-milking",
+"Seven swans a-swimming",
+"Six geese a-laying",
+"Five golden rings",
+"Four calling birds",
+"Three french hens",
+"Two turtle doves, and",
+"A partridge in a pear tree",
+]
+
+const version11 = [
+        "On the eleventh day of Christmas, my true love sent to me",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three french hens",
+        "Two turtle doves, and",
+        "A partridge in a pear tree",
+]
+
+const version12 = [
+    "On the twelfth day of Christmas, my true love sent to me",
+    "Twelve drummers drumming",
+    "Eleven pipers piping",
+    "Ten lords a-leaping",
+    "Nine ladies dancing",
+    "Eight maids a-milking",
+    "Seven swans a-swimming",
+    "Six geese a-laying",
+    "Five golden rings",
+    "Four calling birds",
+    "Three french hens",
+    "Two turtle doves, and",
+    "A partridge in a pear tree",
+]
+
+const pole = [version1, version2, version3, version4, version5, version6, version7, version8, version9, version10, version11, version12]
+for (let i = 0; i <= pole.length; i++){
+    //console.log(pole.length )
+    //console.log(pole)
+}
+console.log(pole)
+
+//console.log(pole[0])
+
+pole.forEach(function(item){
+    console.log(item)
+})
+
+pole.forEach(function(){
+
+})
+//console.log(pole.length)
+
+//console.log(pole[0])
+
