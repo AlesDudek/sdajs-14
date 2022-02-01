72577f70cd67b2bf04538896e6966933dee2fe92
Aleš <mit.mit@seznam.cz>
2022-02-01 12:26:46 +0100

    domaci ukol

--- ala/Task_1/script.js
+++ ala/Task_1/script.js
@@ -131,22 +131,9 @@ const version12 = [
 ]
 
 const pole = [version1, version2, version3, version4, version5, version6, version7, version8, version9, version10, version11, version12]
-for (let i = 0; i <= pole.length; i++){
-    //console.log(pole.length )
-    //console.log(pole)
-}
-console.log(pole)
+console.log(pole.length)
 
-//console.log(pole[0])
+console.log(pole[5][4])
 
-pole.forEach(function(item){
-    console.log(item)
-})
-
-pole.forEach(function(){
-
-})
-//console.log(pole.length)
-
-//console.log(pole[0])
+console.log(pole[0])
 
